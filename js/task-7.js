34bd23d00ba8673629c6c2b46003b38c5127f874
Ekateryna991 <kateponomarenko991@gmail.com>
2020-10-06 22:39:33 +0300

    task-6-8

--- js/task-7.js
+++ js/task-7.js
@@ -86,22 +86,8 @@ const users = [
 ];
 
 // Write code under this line
-const getUsersWithAge = (array, min, max) =>
-  array
-    .filter(({ age }) => (age = age > min && age < max))
-    .map(({ name, email }) => ({ name, email }));
+const calculateTotalBalance = (array) =>
+  array.reduce((total, { balance }) => total + balance, 0);
 
-console.log(getUsersWithAge(users, 20, 30));
-/* [
-    { name: 'Ross Vazquez', email: 'rossvazquez@xinware.com' },
-    { name: 'Elma Head', email: 'elmahead@omatom.com' },
-    { name: 'Carey Barr', email: 'careybarr@nurali.com' }
-] */
-
-console.log(getUsersWithAge(users, 30, 40));
-/* [
-    { name: 'Moore Hensley', email: 'moorehensley@indexia.com' },
-    { name: 'Sharlene Bush', email: 'sharlenebush@tubesys.com' },
-    { name: 'Blackburn Dotson', email: 'blackburndotson@furnigeer.com' },
-    { name: 'Sheree Anthony', email: 'shereeanthony@kog.com' }
-] */
+console.log(calculateTotalBalance(users));
+// 20916
